c5c4fcc66699708adf24c7fe339182d27647567b
Brandon Garate <brandongarate177@gmail.com>
2025-11-20 17:26:47 -0800

    Removed uneserray thingy

--- frontend/src/components/RecordControls.tsx
+++ frontend/src/components/RecordControls.tsx
@@ -92,14 +92,7 @@ const RecordControls: React.FC<Props> = ({ setStatus, setResults }) => {
         )}
       </button>
       
-      <div className="text-xs text-gray-600 bg-blue-50 p-2 rounded-md border border-blue-100">
-        <div className="flex items-center space-x-1">
-          <svg className="w-3 h-3 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-          <span>Microphone access will be requested when starting</span>
-        </div>
-      </div>
+ 
     </div>
   );
 };
